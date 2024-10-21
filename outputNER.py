355f180355c03868485cc81b543270d7191f17cc
JStuckless <jason.stuckless1@ontariotechu.net>
2024-10-21 14:59:35 -0400

    Add files via upload

--- outputNER.py
+++ outputNER.py
@@ -5,28 +5,30 @@ import spacy
 from spacy import displacy
 import Transcript
 
-# import transcript file and remove timestamps and speaker from text
-transcript = Transcript.cleanTranscript(Transcript.getText("meeting.docx"), 0, 0)
+def createNerOutput (filepath):
+    # import transcript file and remove timestamps and speaker from text
+    transcript = Transcript.cleanTranscript(Transcript.getText(filepath), 0, 0)
 
-# prepare spacy pipeline and assign it the transcript file to analyse
-nlp = spacy.load("en_core_web_lg")
-doc = nlp(transcript)
+    # prepare spacy pipeline and assign it the transcript file to analyse
+    nlp = spacy.load("en_core_web_lg")
+    doc = nlp(transcript)
 
-# remove files if they already exist
-if os.path.isfile("transcript.html"):
-    os.remove("transcript.html")
-if os.path.isfile("transcriptDependencies.html"):
-    os.remove("transcriptDependencies.html")
+    # remove files if they already exist
+    if os.path.isfile("transcript.html"):
+        os.remove("transcript.html")
+    if os.path.isfile("transcriptDependencies.html"):
+        os.remove("transcriptDependencies.html")
 
-# create HTML document of transcript with NER items highlighted and labelled
-# options = {"ents": ["NOUN"]} , options=options
-# (WIP: trying to show more highlighted types on NER visualization)
-html = displacy.render(doc, style="ent", page=True)
-f = open("transcript.html", "x")
-f.write(html)
+    # create HTML document of transcript with NER items highlighted and labelled
+    # options = {"ents": ["NOUN"]} , options=options
+    # (WIP: trying to show more highlighted types on NER visualization)
+    html = displacy.render(doc, style="ent", page=True)
+    f = open("transcript.html", "x")
+    f.write(html)
 
-# create HTML document of transcript with sentence dependencies
-sentences = list(doc.sents)
-html2 = displacy.render(sentences, style="dep", page=True)
-f2 = open("transcriptDependencies.html", "x")
-f2.write(html2)
+    # create HTML document of transcript with sentence dependencies
+    sentences = list(doc.sents)
+    html2 = displacy.render(sentences, style="dep", page=True)
+    f2 = open("transcriptDependencies.html", "x")
+    f2.write(html2)
+

dcffe3d553aae76116d478f5c0702f2b9fcb51ec
DaiAndy <80484420+DaiAndy@users.noreply.github.com>
2024-11-12 22:48:50 -0500

    added gpt summarization call to pam.py
    added the import of GPT.py into pam.py, and added a function call to create the summarization from GPT

--- pam.py
+++ pam.py
@@ -6,6 +6,7 @@ import ActionableItems
 import Minutes
 import Redaction
 import Transcript
+import GPT as gpt
 
 # HOW IS pam.py CALLING THE GUI AND RETRIEVING THE FILEPATH?????
 # This does not run right now
@@ -19,6 +20,9 @@ filepath = "transcript02.docx"
 import docx2txt
 redactedTranscript = docx2txt.process(filepath)
 # REMOVE LATER
+
+# call to create a summaraization of the transcript with GPT
+gpt.gptSummarization(filepath)
 
 # Create file name and generate minutes document and summary terminal output
 minutesFilename = Transcript.extractFilenameDate(filepath) + "_minutes.docx"
